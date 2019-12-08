77c9d32434297a29d717dee38ce74a1ea5a16f64
Effanville <matt.egginton@gmail.com>
2019-12-08 15:26:10 +0000

    Add Closing query message box.

--- FinancePortfolioDatabase/GUI/MainWindow.xaml.cs
+++ FinancePortfolioDatabase/GUI/MainWindow.xaml.cs
@@ -1,7 +1,7 @@
 ﻿using GUIAccessorFunctions;
-using FinancialStructures.ReportingStructures;
-using System.Windows;
 using GlobalHeldData;
+using System.Windows;
+using System.Windows.Forms;
 
 namespace FinanceWindows
 {
@@ -22,6 +22,35 @@ namespace FinanceWindows
         {
             var helpwindow = new HelpWindow();
             helpwindow.Show();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            string msg = "Data may not be saved. Would you like to save before closing?";
+            MessageBoxResult result =
+                  System.Windows.MessageBox.Show(
+                    msg,
+                    $"Closing {Title}.",
+                    MessageBoxButton.YesNoCancel,
+                    MessageBoxImage.Warning);
+            if (result == MessageBoxResult.Yes)
+            {
+                SaveFileDialog saving = new SaveFileDialog();
+                if (saving.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    //if (!File.Exists(saving.FileName))
+                    {
+                        DatabaseAccessor.SetFilePath(saving.FileName);
+                    }
+                }
+
+                DatabaseAccessor.SavePortfolio();
+                saving.Dispose();
+            }
+            if (result == MessageBoxResult.Cancel)
+            {
+                e.Cancel = true;
+            }
         }
     }
 }
