98de8f35ba16d2189f7bb99b8fab5ac6233ec8ce
Effanville <matt.egginton@gmail.com>
2020-01-24 21:26:34 +0000

    Add command line program

---
+++ FPDconsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FPDconsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
