4f17b182f3ddaededa2785fcd54ef21da53c4a1a
Effanville <>
2020-02-09 20:08:11 +0000

    First modifications to command line for downloading.

--- FPDconsole/Program.cs
+++ FPDconsole/Program.cs
@@ -1,12 +1,39 @@
-﻿using System;
+﻿using FinancialStructures.Database;
+using FinancialStructures.FinanceStructures;
+using FinancialStructures.ReportingStructures;
+using System;
+using System.Threading.Tasks;
 
 namespace FPDconsole
 {
     class Program
     {
+        static void WriteReports(ErrorReports reports)
+        {
+            foreach (var report in reports.GetReports())
+            {
+                Console.WriteLine(report.ToString());
+            }
+        }
+
+        public static Portfolio portfolio = new Portfolio();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args.Length == 0)
+            {
+                
+            }
+            Console.WriteLine("Program Started");
+            var reports = new ErrorReports();
+            portfolio.LoadPortfolio("", reports);
+
+            DoStuff(reports).Wait();
+            Console.WriteLine("Program finished");
+        }
+
+        public static async Task DoStuff(ErrorReports reports)
+        {
+            await Download.DownloadSecurityLatest(new Security("hi", "low", "GBP", "https://markets.ft.com/data/funds/tearsheet/summary?s=gb00b2pljn71:gbx"), (reports) => WriteReports(reports), reports);
         }
     }
 }
