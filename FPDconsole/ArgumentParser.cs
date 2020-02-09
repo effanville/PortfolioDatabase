0c78d902c6e308ca2b4bea73a4802ec8737b82f6
Effanville <>
2020-02-09 20:07:52 +0000

    Streamline download code and remove old unused methods.
    Note: morningstar currently does not work. Suspect this is them rejecting my requests
          but cannot rule out a malformed request on my behalf.

---
+++ FPDconsole/ArgumentParser.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace FPDconsole
+{
+    public static class ArgumentParser
+    {
+        public static List<TextToken> Parse(string[] args)
+        {
+            List<TextToken> tokens = new List<TextToken>();
+            if (args.Length > 0)
+            {
+                tokens.Add(PrepareFilePath(args[0]));
+            }
+            else { }
+
+            return tokens;
+        }
+
+        public static TextToken PrepareFilePath(string expectedFilePath)
+        {
+            if (File.Exists(expectedFilePath))
+            {
+                return new TextToken(TokenType.FilePath, expectedFilePath);
+            }
+
+            return new TextToken(TokenType.Error, expectedFilePath);
+        }
+    }
+
+    public enum TokenType
+    {
+        FilePath,
+        Argument,
+        Parameter,
+        Error
+    }
+
+    public class TextToken
+    {
+        public TokenType sort;
+        public string field;
+
+        public TextToken(TokenType type, string value)
+        { 
+            sort = type;
+            field = value;
+        }
+    }
+}
--- FinancialStructures/Portfolio/Portfolio.Download.cs
+++ FinancialStructures/Portfolio/Portfolio.Download.cs
@@ -9,6 +9,16 @@ using System.Threading.Tasks;
 
 namespace FinancialStructures.Database
 {
+    public enum WebsiteType
+    {
+        Morningstar,
+        Yahoo,
+        Google,
+        Bloomberg,
+        TrustNet,
+        FT,
+        NotImplemented
+    }
     public static class Download
     {
         private static string Pence = "GBX";
@@ -24,302 +34,34 @@ namespace FinancialStructures.Database
             return Uri.IsWellFormedUriString(address, UriKind.Absolute);
         }
 
-        private static bool IsMorningstarAddress(string address)
+        private static WebsiteType AddressType(string address)
         {
-            return address.Contains("morningstar");
-        }
-
-        private static bool IsYahooAddress(string address)
-        {
-            return address.Contains("yahoo");
-        }
-
-        private static bool IsGoogleAddress(string address)
-        {
-            return address.Contains("google");
-        }
-
-        private static bool IsTrustNetAddress(string address)
-        {
-            return address.Contains("trustnet");
-        }
-
-        private static bool IsBloomBergAddress(string address)
-        {
-            return address.Contains("bloomberg.com");
-        }
-
-        private static async Task<string> DownloadFromURL(string url, ErrorReports reports)
-        {
-            if (string.IsNullOrEmpty(url))
+            if (address.Contains("morningstar"))
             {
-                return string.Empty;
+                return WebsiteType.Morningstar;
+            }
+            if (address.Contains("yahoo"))
+            {
+                return WebsiteType.Yahoo;
+            }
+            if (address.Contains("google"))
+            {
+                return WebsiteType.Google;
+            }
+            if (address.Contains("trustnet"))
+            {
+                return WebsiteType.TrustNet;
+            }
+            if (address.Contains("bloomberg"))
+            {
+                return WebsiteType.Bloomberg;
+            }
+            if (address.Contains("markets.ft"))
+            {
+                return WebsiteType.FT;
             }
 
-            string newUrl = Uri.EscapeUriString(url);
-            string output = string.Empty;
-            if (IsValidWebAddress(newUrl))
-            {
-                try
-                {
-                    using (HttpResponseMessage response = await client.GetAsync(newUrl).ConfigureAwait(false))
-                    using (HttpContent content = response.Content)
-                    {
-                        // ... Read the string.
-                        string result = await content.ReadAsStringAsync().ConfigureAwait(false);
-
-                        // ... Display the result.
-                        if (result != null && result.Length >= 50)
-                        {
-                            output = result;
-                        }
-                    }
-                }
-                catch (Exception ex)
-                {
-                    reports.AddError($"Failed to download from url {url}. Reason : {ex.Message}");
-                    return output;
-                }
-            }
-
-            return output;
-        }
-
-        private static bool ProcessDownloadString(string url, string data, ErrorReports reports, out double value)
-        {
-            value = double.NaN;
-
-            if (IsMorningstarAddress(url))
-            {
-                value = ProcessFromMorningstar(data);
-                if (double.IsNaN(value))
-                {
-                    reports.AddError($"Could not download data from morningstar url: {url}");
-                    return false;
-                }
-                return true;
-            }
-            if (IsYahooAddress(url))
-            {
-                value = ProcessFromYahoo(data);
-                if (double.IsNaN(value))
-                {
-                    reports.AddError($"Could not download data from Yahoo url: {url}");
-                    return false;
-                }
-                return true;
-            }
-            if (IsGoogleAddress(url))
-            {
-                value = ProcessFromGoogle(data);
-                if (double.IsNaN(value))
-                {
-                    reports.AddError($"Could not download data from google url: {url}");
-                    return false;
-                }
-                return true;
-            }
-            if (IsTrustNetAddress(url))
-            {
-                value = ProcessFromTrustNet(data);
-                if (double.IsNaN(value))
-                {
-                    reports.AddError($"Could not download data from Trustnet url: {url}");
-                    return false;
-                }
-                return true;
-            }
-            if (IsBloomBergAddress(url))
-            {
-                value = ProcessFromBloomBerg(data);
-                if (double.IsNaN(value))
-                {
-                    reports.AddError($"Could not download data from BloomBerg url: {url}");
-                    return false;
-                }
-                return true;
-            }
-
-            reports.AddError($"Url not of a currently implemented downloadable type: {url}");
-            return false;
-        }
-
-        private static double ProcessFromMorningstar(string data)
-        {
-            bool continuer(char c)
-            {
-                if (char.IsDigit(c) || c == '.')
-                {
-                    return true;
-                }
-
-                return false;
-            };
-
-            int penceValue = data.IndexOf(Pence);
-            if (penceValue != -1)
-            {
-                string containsNewValue = data.Substring(Pence.Length + penceValue, 20);
-
-                var digits = containsNewValue.SkipWhile(c => !char.IsDigit(c)).TakeWhile(continuer).ToArray();
-
-                var str = new string(digits);
-                if (string.IsNullOrEmpty(str))
-                {
-                    return double.NaN;
-                }
-                double i = double.Parse(str);
-                i = i / 100;
-                return i;
-            }
-            string searchName = "<td class=\"line text\">" + Pounds;
-            int poundsValue = data.IndexOf(searchName);
-            if (poundsValue != -1)
-            {
-                string containsNewValue = data.Substring(searchName.Length + poundsValue, 20);
-                var digits = containsNewValue.SkipWhile(c => !char.IsDigit(c)).TakeWhile(continuer).ToArray();
-
-                var str = new string(digits);
-                if (string.IsNullOrEmpty(str))
-                {
-                    return double.NaN;
-                }
-                double i = double.Parse(str);
-
-                return i;
-            }
-
-            return double.NaN;
-        }
-
-        private static double ProcessFromYahoo(string data)
-        {
-            bool continuer(char c)
-            {
-                if (char.IsDigit(c) || c == '.' || c == ',')
-                {
-                    return true;
-                }
-
-                return false;
-            };
-            string searchString = "data-reactid=\"33\"><span class=\"Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)\" data-reactid=\"34\">";
-            int poundsValue = data.IndexOf(searchString);
-            if (poundsValue != -1)
-            {
-                string containsNewValue = data.Substring(poundsValue + searchString.Length, 20);
-
-                var digits = containsNewValue.SkipWhile(c => !char.IsDigit(c)).TakeWhile(continuer).ToArray();
-
-                var str = new string(digits);
-                if (string.IsNullOrEmpty(str))
-                {
-                    return double.NaN;
-                }
-                double i = double.Parse(str);
-                if (data.Contains("GBp"))
-                {
-                    i = i / 100.0;
-                }
-                return i;
-            }
-
-            return double.NaN;
-        }
-
-        private static double ProcessFromGoogle(string data)
-        {
-            bool continuer(char c)
-            {
-                if (char.IsDigit(c) || c == '.')
-                {
-                    return true;
-                }
-
-                return false;
-            };
-            string searchName = "regularMarketPrice";
-            int penceValue = data.IndexOf(searchName);
-            if (penceValue != -1)
-            {
-                string containsNewValue = data.Substring(searchName.Length + penceValue, 20);
-
-                var digits = containsNewValue.SkipWhile(c => !char.IsDigit(c)).TakeWhile(continuer).ToArray();
-
-                var str = new string(digits);
-                if (string.IsNullOrEmpty(str))
-                {
-                    return double.NaN;
-                }
-                double i = double.Parse(str);
-                return i;
-            }
-
-            return double.NaN;
-        }
-
-        private static double ProcessFromTrustNet(string data)
-        {
-            bool continuer(char c)
-            {
-                if (char.IsDigit(c) || c == '.')
-                {
-                    return true;
-                }
-
-                return false;
-            };
-            string searchName = "regularMarketPrice";
-            int penceValue = data.IndexOf(searchName);
-            if (penceValue != -1)
-            {
-                string containsNewValue = data.Substring(searchName.Length + penceValue, 20);
-
-                var digits = containsNewValue.SkipWhile(c => !char.IsDigit(c)).TakeWhile(continuer).ToArray();
-
-                var str = new string(digits);
-                if (string.IsNullOrEmpty(str))
-                {
-                    return double.NaN;
-                }
-                double i = double.Parse(str);
-                return i;
-            }
-
-            return double.NaN;
-        }
-
-
-
-        private static double ProcessFromBloomBerg(string data)
-        {
-            bool continuer(char c)
-            {
-                if (char.IsDigit(c) || c == '.')
-                {
-                    return true;
-                }
-
-                return false;
-            };
-            string searchName = "<span class=\"priceText__1853e8a5\">";
-            int penceValue = data.IndexOf(searchName);
-            if (penceValue != -1)
-            {
-                string containsNewValue = data.Substring(searchName.Length + penceValue, 20);
-
-                var digits = containsNewValue.SkipWhile(c => !char.IsDigit(c)).TakeWhile(continuer).ToArray();
-
-                var str = new string(digits);
-                if (string.IsNullOrEmpty(str))
-                {
-                    return double.NaN;
-                }
-                double i = double.Parse(str);
-                return i;
-            }
-
-            return double.NaN;
+            return WebsiteType.NotImplemented;
         }
 
         public async static Task DownloadPortfolioLatest(Portfolio portfo, Action<ErrorReports> updateReports, ErrorReports reports)
@@ -418,6 +160,190 @@ namespace FinancialStructures.Database
             }
             sector.TryAddData(DateTime.Today, value, reports);
             updateReports(reports);
+        }
+
+        public static async Task<string> DownloadFromURL(string url, ErrorReports reports)
+        {
+            if (string.IsNullOrEmpty(url))
+            {
+                return string.Empty;
+            }
+
+            string newUrl = Uri.EscapeUriString(url);
+            string output = string.Empty;
+            if (IsValidWebAddress(newUrl))
+            {
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync(newUrl).ConfigureAwait(false);
+                    response.EnsureSuccessStatusCode();
+                    string result = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+                    if (result != null)
+                    {
+                        output = result;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    reports.AddError($"Failed to download from url {url}. Reason : {ex.Message}");
+                    return output;
+                }
+            }
+
+            return output;
+        }
+
+        private static bool ProcessDownloadString(string url, string data, ErrorReports reports, out double value)
+        {
+            value = double.NaN;
+            switch (AddressType(url))
+            {
+                case WebsiteType.FT:
+                    {
+                        value = ProcessFromFT(data);
+                        if (double.IsNaN(value))
+                        {
+                            reports.AddError($"Could not download data from morningstar url: {url}");
+                            return false;
+                        }
+                        return true;
+                    }
+                case WebsiteType.Yahoo:
+                    {
+                        value = ProcessFromYahoo(data);
+                        if (double.IsNaN(value))
+                        {
+                            reports.AddError($"Could not download data from Yahoo url: {url}");
+                            return false;
+                        }
+                        return true;
+                    }
+                default:
+                case WebsiteType.Morningstar:
+                case WebsiteType.Google:
+                case WebsiteType.TrustNet:
+                case WebsiteType.Bloomberg:
+                    reports.AddError($"Url not of a currently implemented downloadable type: {url}");
+                    return false;
+            }
+        }
+
+        private static double ProcessFromMorningstar(string data)
+        {
+            bool continuer(char c)
+            {
+                if (char.IsDigit(c) || c == '.')
+                {
+                    return true;
+                }
+
+                return false;
+            };
+
+            int penceValue = data.IndexOf(Pence);
+            if (penceValue != -1)
+            {
+                string containsNewValue = data.Substring(Pence.Length + penceValue, 20);
+
+                var digits = containsNewValue.SkipWhile(c => !char.IsDigit(c)).TakeWhile(continuer).ToArray();
+
+                var str = new string(digits);
+                if (string.IsNullOrEmpty(str))
+                {
+                    return double.NaN;
+                }
+                double i = double.Parse(str);
+                i = i / 100;
+                return i;
+            }
+            string searchName = "<td class=\"line text\">" + Pounds;
+            int poundsValue = data.IndexOf(searchName);
+            if (poundsValue != -1)
+            {
+                string containsNewValue = data.Substring(searchName.Length + poundsValue, 20);
+                var digits = containsNewValue.SkipWhile(c => !char.IsDigit(c)).TakeWhile(continuer).ToArray();
+
+                var str = new string(digits);
+                if (string.IsNullOrEmpty(str))
+                {
+                    return double.NaN;
+                }
+                double i = double.Parse(str);
+
+                return i;
+            }
+
+            return double.NaN;
+        }
+
+        private static double ProcessFromYahoo(string data)
+        {
+            bool continuer(char c)
+            {
+                if (char.IsDigit(c) || c == '.' || c == ',')
+                {
+                    return true;
+                }
+
+                return false;
+            };
+            string searchString = "data-reactid=\"33\"><span class=\"Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)\" data-reactid=\"34\">";
+            int poundsValue = data.IndexOf(searchString);
+            if (poundsValue != -1)
+            {
+                string containsNewValue = data.Substring(poundsValue + searchString.Length, 20);
+
+                var digits = containsNewValue.SkipWhile(c => !char.IsDigit(c)).TakeWhile(continuer).ToArray();
+
+                var str = new string(digits);
+                if (string.IsNullOrEmpty(str))
+                {
+                    return double.NaN;
+                }
+                double i = double.Parse(str);
+                if (data.Contains("GBp"))
+                {
+                    i = i / 100.0;
+                }
+                return i;
+            }
+
+            return double.NaN;
+        }
+
+        private static double ProcessFromFT(string data)
+        {
+            bool continuer(char c)
+            {
+                if (char.IsDigit(c) || c == '.' || c == ',')
+                {
+                    return true;
+                }
+
+                return false;
+            };
+            string searchString = "Price (GBP)";
+            int poundsValue = data.IndexOf(searchString);
+            if (poundsValue != -1)
+            {
+                string containsNewValue = data.Substring(poundsValue + searchString.Length, 200);
+
+                var digits = containsNewValue.SkipWhile(c => !char.IsDigit(c)).TakeWhile(continuer).ToArray();
+
+                var str = new string(digits);
+                if (string.IsNullOrEmpty(str))
+                {
+                    return double.NaN;
+                }
+                double i = double.Parse(str);
+                if (data.Contains("GBp"))
+                {
+                    i = i / 100.0;
+                }
+                return i;
+            }
+
+            return double.NaN;
         }
     }
 }
