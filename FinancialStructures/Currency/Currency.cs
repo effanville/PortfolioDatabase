c85cf433266f91bb316b6be23fc0029237416e69
Effanville <matt.egginton@gmail.com>
2020-02-20 22:00:11 +0000

    Modify currency to use a standard base class.

--- FinancialStructures/Currency/Currency.cs
+++ FinancialStructures/Currency/Currency.cs
@@ -2,96 +2,38 @@
 
 namespace FinancialStructures.FinanceStructures
 {
-    /// <summary>
-    /// Acts as an overall change of an area of funds.
-    /// </summary>
-    /// <example>
-    /// e.g. FTSE100 or MSCI-Asia
-    /// </example>
-    public partial class Currency
+    internal class Currency : SingleValueDataList
     {
-        /// <summary>
-        /// The name of the sector. 
-        /// </summary>
-        /// <remarks>
-        /// These names must be unique
-        /// </remarks>
-        private string fName;
-
-        /// <summary>
-        /// This should only be used for serialisation.
-        /// </summary>
-        public string Name
+        public new Currency Copy()
         {
-            get => fName;
-            set => fName = value;
+            return new Currency(Name, Values);
         }
+        internal Currency(string name, string url)
+            : base(name, url)
+        { }
 
-        private string fUrl;
-
-        public string Url
-        {
-            get => fUrl;
-            set => fUrl = value;
-        }
-
-        /// <summary>
-        /// The values of the sector.
-        /// </summary>
-        private TimeList fValues;
-
-        /// <summary>
-        /// This should only be used for serialisation.
-        /// </summary>
-        public TimeList Values
-        {
-            get => fValues;
-            set => fValues = value;
-        }
+        private Currency(string name, TimeList values)
+    : base(name, values)
+        { }
 
         /// <summary>
         /// default constructor.
         /// </summary>
-        public Currency()
+        private Currency()
+                : base()
         { }
 
         /// <summary>
         /// Creates a new instance of a sector.
         /// </summary>
-        public Currency(string name)
+        private Currency(string name)
+            : base(name)
         {
-            fName = name;
-            fValues = new TimeList();
-        }
-
-        public Currency(string name, string url)
-        {
-            fName = name;
-            fUrl = url;
-            fValues = new TimeList();
-        }
-
-        private Currency(string name, TimeList values)
-        {
-            fName = name;
-            fValues = values;
         }
 
         private Currency(string name, string url, TimeList values)
+            : base(name, url, values)
         {
-            fName = name;
-            fUrl = url;
-            fValues = values;
-        }
-
-        public Currency Copy()
-        {
-            return new Currency(fName, fValues);
-        }
-
-        public bool Any()
-        {
-            return fValues.Any();
         }
     }
 }
