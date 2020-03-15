0d0c93eb49426b05abc1cfe78c990c3b3ea7a084
Effanville <matt.egginton@gmail.com>
2020-03-15 21:27:32 +0000

    Program now catches unhandled exceptions and may not crash.

--- FinancePortfolioDatabase/App.xaml.cs
+++ FinancePortfolioDatabase/App.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System.Globalization;
+﻿using System;
+using System.Globalization;
 using System.Windows;
 using System.Windows.Media.Animation;
+using System.Windows.Threading;
 
 namespace FinancePortfolioDatabase
 {
@@ -23,6 +25,29 @@ namespace FinancePortfolioDatabase
                 typeof(FrameworkElement),
                 new FrameworkPropertyMetadata(
                 System.Windows.Markup.XmlLanguage.GetLanguage(CultureInfo.CurrentUICulture.IetfLanguageTag)));
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
+        }
+
+        public void Application_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            if (true)
+            {
+                //Handling the exception within the UnhandledExcpeiton handler.
+                MessageBox.Show(e.Exception.Message + Environment.NewLine + e.Exception.StackTrace, "Exception Caught", MessageBoxButton.OK, MessageBoxImage.Error);
+                e.Handled = true;
+            }
+            else
+            {
+                //If you do not set e.Handled to true, the application will close due to crash.
+                MessageBox.Show("Application is going to close! ", "Uncaught Exception");
+                e.Handled = false;
+            }
+        }
+
+        void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            Exception ex = e.ExceptionObject as Exception;
+            MessageBox.Show(ex.Message, "Uncaught Thread Exception", MessageBoxButton.OK, MessageBoxImage.Error);
         }
     }
 }
