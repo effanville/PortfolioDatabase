582ee5a676093ed97e04a60c987ed9947e0c328a
Effanville <matt.egginton@gmail.com>
2020-05-09 15:31:11 +0100

    Add generic converters to Common code.

---
+++ FinancePortfolioDatabase/UICommon/Converters/ArrayMultiValueConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace UICommon.Converters
+{
+	/// <summary>
+	/// Converter to convert an array of objects into a single object (and not back again)
+	/// </summary>
+	public class ArrayMultiValueConverter : MarkupExtension, IMultiValueConverter
+	{
+		private static ArrayMultiValueConverter sConverter;
+
+		public override object ProvideValue(IServiceProvider serviceProvider)
+		{
+			if (sConverter == null)
+			{
+				sConverter = new ArrayMultiValueConverter();
+			}
+
+			return sConverter;
+		}
+
+		public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+		{
+			return values.Clone();
+		}
+
+		public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
