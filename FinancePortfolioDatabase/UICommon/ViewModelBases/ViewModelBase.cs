18713cf83db72a752596a57678a9a555af8d22f5
Effanville <>
2020-05-09 15:39:54 +0100

    Force dateTime to be UK cultlure if english culture is used

--- FinancePortfolioDatabase/UICommon/ViewModelBases/ViewModelBase.cs
+++ FinancePortfolioDatabase/UICommon/ViewModelBases/ViewModelBase.cs
@@ -8,16 +8,38 @@ namespace UICommon.ViewModelBases
     public abstract class ViewModelBase<T> : PropertyChangedBase where T : class
     {
         /// <summary>
+        /// The data to be used in this view model.
+        /// </summary>
+        public T DataStore
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
         /// Any string to use to display in a header or a title of a UI element.
         /// </summary>
-        public virtual string Header { get; }
+        public virtual string Header
+        {
+            get;
+        }
 
         /// <summary>
         /// Whether this element should be able to be closed.
         /// </summary>
-        public virtual bool Closable { get { return false; } }
+        public virtual bool Closable
+        {
+            get
+            {
+                return false;
+            }
+        }
 
-        public virtual Action<object> LoadSelectedTab { get; set; }
+        public virtual Action<object> LoadSelectedTab
+        {
+            get;
+            set;
+        }
 
         public ViewModelBase(string header)
         {
@@ -30,10 +52,19 @@ namespace UICommon.ViewModelBases
             LoadSelectedTab = loadTab;
         }
 
-        public virtual void UpdateData(T portfolio, Action<object> removeTab)
+        public virtual void UpdateData(T dataToDisplay, Action<object> removeTab)
         {
+            DataStore = null;
+            DataStore = dataToDisplay;
         }
 
-        public abstract void UpdateData(T portfolio);
+        /// <summary>
+        /// Mechanism to update the data 
+        /// </summary>
+        public virtual void UpdateData(T dataToDisplay)
+        {
+            DataStore = null;
+            DataStore = dataToDisplay;
+        }
     }
 }
