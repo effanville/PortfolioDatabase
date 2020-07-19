abd68a793e377e9aa581d6439ff10c27eb19d477
Effanville <>
2020-07-19 16:01:25 +0100

    Create Test constructors for finance objects

---
+++ PAD_UnitTests/TestDatabaseConstructor/BankAccountConstructor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using FinancialStructures.FinanceStructures;
+using FinancialStructures.NamingStructures;
+using StructureCommon.DataStructures;
+
+namespace FinancialStructures_UnitTests.TestDatabaseConstructor
+{
+    public class BankAccountConstructor
+    {
+        public CashAccount item;
+
+        public BankAccountConstructor(string company, string name, string currency = null, string url = null, string sectors = null)
+        {
+            var names = new NameData(company, name, currency, url);
+            names.SectorsFlat = sectors;
+            item = new CashAccount(names);
+        }
+
+        public BankAccountConstructor WithData(DateTime date, double price)
+        {
+            item.Amounts.Values.Add(new DailyValuation(date, price));
+
+            return this;
+        }
+    }
+}
