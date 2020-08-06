360002b7839f93efc7b233609dacb19003cb061a
Effanville <matt.egginton@gmail.com>
2020-08-06 22:07:21 +0100

    Add default API tests.

---
+++ PAD_UnitTests/PortfolioAPI/DownloadTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using FinancialStructures.NamingStructures;
+using FinancialStructures.PortfolioAPI;
+using NUnit.Framework;
+
+namespace FinancialStructures_UnitTests.PortfolioAPI
+{
+    public sealed class DownloadTests
+    {
+        [TestCase("https://uk.finance.yahoo.com/quote/VWRL.L")]
+        [TestCase("https://markets.ft.com/data/funds/tearsheet/summary?s=gb00b4khn986:gbx")]
+        public void CanDownload(string url)
+        {
+            double value = 0;
+            Action<double> getValue = v => value = v;
+            PortfolioDataUpdater.DownloadLatestValue(new NameData("company", "name", url: url), getValue, null).Wait();
+
+            Assert.AreNotEqual(0, value);
+        }
+    }
+}
