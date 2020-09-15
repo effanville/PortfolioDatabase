223cc728756e7bda7e1e6a3553c8809fd00776fd
Effanville <>
2020-09-15 22:26:52 +0100

    Add Total Value tests.

--- PAD_UnitTests/TestDatabaseConstructor/DatabaseConstructor.cs
+++ PAD_UnitTests/TestDatabaseConstructor/DatabaseConstructor.cs
@@ -53,9 +53,34 @@ namespace FinancialStructures_UnitTests.TestDatabaseConstructor
             }
         }
 
+        public DatabaseConstructor WithSecondaryFromType(AccountType acctype)
+        {
+            switch (acctype)
+            {
+                case AccountType.Security:
+                    return WithSecondarySecurity();
+                case AccountType.BankAccount:
+                    return WithSecondaryBankAccount();
+                default:
+                    return null;
+            }
+        }
+
         public DatabaseConstructor WithDefaultSecurity()
         {
             return WithSecurityFromNameAndData(DefaultSecurityCompany, DefaultSecurityName, dates: DefaultSecurityDates, sharePrice: DefaultSecurityUnitPrices, numberUnits: DefaultSecurityShareValues, investment: DefaultSecurityInvestments);
+        }
+
+        public const string SecondarySecurityName = "China Stock";
+        public const string SecondarySecurityCompany = "Prudential";
+        public readonly DateTime[] SecondarySecurityDates = new DateTime[] { new DateTime(2010, 1, 5), new DateTime(2011, 2, 1), new DateTime(2012, 5, 5), new DateTime(2016, 4, 3), new DateTime(2019, 5, 6), new DateTime(2020, 1, 1) };
+        public readonly double[] SecondarySecurityShareValues = new double[] { 2.0, 2.5, 17.3, 22.5, 22.7, 25.5 };
+        public readonly double[] SecondarySecurityUnitPrices = new double[] { 1010.0, 1110.0, 1215.2, 900.6, 1770.7, 1001.1 };
+        public readonly double[] SecondarySecurityInvestments = new double[] { 1, 0.0, 1, 0.0, 0.0, 0.0 };
+
+        public DatabaseConstructor WithSecondarySecurity()
+        {
+            return WithSecurityFromNameAndData(SecondarySecurityCompany, SecondarySecurityName, dates: SecondarySecurityDates, sharePrice: SecondarySecurityUnitPrices, numberUnits: SecondarySecurityShareValues, investment: SecondarySecurityInvestments);
         }
 
         public string DefaultBankAccountName = "Current";
@@ -66,6 +91,17 @@ namespace FinancialStructures_UnitTests.TestDatabaseConstructor
         public DatabaseConstructor WithDefaultBankAccount()
         {
             return WithBankAccountFromNameAndData(DefaultBankAccountCompany, DefaultBankAccountName, date: DefaultBankAccountDates, value: DefaultBankAccountValues);
+        }
+
+
+        public string SecondaryBankAccountName = "Current";
+        public string SecondaryBankAccountCompany = "Halifax";
+        public DateTime[] SecondaryBankAccountDates = new DateTime[] { new DateTime(2010, 1, 1), new DateTime(2011, 1, 1), new DateTime(2012, 5, 1), new DateTime(2015, 4, 3), new DateTime(2018, 5, 6), new DateTime(2020, 1, 1) };
+        public double[] SecondaryBankAccountValues = new double[] { 1100.0, 2100.0, 1125.2, 900.6, 770.7, 1001.1 };
+
+        public DatabaseConstructor WithSecondaryBankAccount()
+        {
+            return WithBankAccountFromNameAndData(SecondaryBankAccountCompany, SecondaryBankAccountName, date: SecondaryBankAccountDates, value: SecondaryBankAccountValues);
         }
 
         public DatabaseConstructor WithSecurityFromName(string company, string name, string currency = null, string url = null, string sectors = null)
