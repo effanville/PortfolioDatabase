66c9cbcf9f8c7d1771054d405413aa6b3680c9f0
Effanville <>
2020-11-01 21:41:00 +0000

    Add Area for event to occur from.

---
+++ FinancialStructures/Portfolio/EventAction.cs
@@ -0,0 +1,12 @@
+﻿namespace FinancialStructures.FinanceInterfaces
+{
+    public enum EventAction
+    {
+        AddingAccount,
+        EditingAccountMetadata,
+        AddingData,
+        EditingData,
+        AccessingMetadata,
+        AccessingData
+    }
+}
