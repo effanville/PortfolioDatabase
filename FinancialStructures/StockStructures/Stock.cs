0fd7b42f0c585cf39af48afd90717e5654eb1079
Matthew Egginton <matt.egginton@gmail.com>
2021-01-10 23:47:09 +0000

    More updates to stock structures.

--- FinancialStructures/StockStructures/Stock.cs
+++ FinancialStructures/StockStructures/Stock.cs
@@ -4,22 +4,29 @@ using FinancialStructures.NamingStructures;
 
 namespace FinancialStructures.StockStructures
 {
+    /// <summary>
+    /// Simulates a Stock.
+    /// </summary>
     public partial class Stock : IStock
     {
         private List<StockDay> fValuations;
-        internal int LastAccessedValuationIndex = 0;
-        /// <summary>
-        /// The name of the stock in question.
-        /// </summary>
+        internal int LastValueIndex = 0;
+
+        /// <inheritdoc/>
+        public string Ticker
+        {
+            get;
+            set;
+        }
+
+        /// <inheritdoc/>
         public NameData Name
         {
             get;
             set;
         }
 
-        /// <summary>
-        /// Values associated to this stock in order earliest -> latest.
-        /// </summary>
+        /// <inheritdoc/>
         public List<StockDay> Valuations
         {
             get
@@ -43,23 +50,20 @@ namespace FinancialStructures.StockStructures
         /// <summary>
         /// Constructor setting basic name information
         /// </summary>
-        public Stock(string company, string name, string url)
+        public Stock(string ticker, string company, string name, string url)
         {
+            Ticker = ticker;
             Name = new NameData(name.Trim(), company.Trim(), "", url.Trim());
             Valuations = new List<StockDay>();
         }
 
-        /// <summary>
-        /// Adds a value to the Stock. Note this does not sort the values, so <see cref="Sort"/> should be called after this.
-        /// </summary>
+        /// <inheritdoc/>
         public void AddValue(DateTime time, double open, double high, double low, double close, double volume)
         {
             Valuations.Add(new StockDay(time, open, high, low, close, volume));
         }
 
-        /// <summary>
-        /// Sorts the values in the stock.
-        /// </summary>
+        /// <inheritdoc/>
         public void Sort()
         {
             Valuations.Sort();
