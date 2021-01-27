44bd5e1879ff0455f7af887539a85fab779b1057
Effanville <matt.egginton@gmail.com>
2021-01-27 22:51:41 +0000

    Update numeric values following changes in Investments in test databases.

--- FinancialStructuresTests/Portfolio/Statistics/IRRTests.cs
+++ FinancialStructuresTests/Portfolio/Statistics/IRRTests.cs
@@ -8,14 +8,14 @@ namespace FinancialStructures.Tests.Database.Statistics
 {
     public sealed class IRRTests
     {
-        [TestCase(TestDatabaseName.TwoSecTwoBank, Totals.All, "2010 /1/1", "2019/1/1", 0.79150390625)]
-        [TestCase(TestDatabaseName.TwoSecTwoBank, Totals.All, "2011/1/1", "2019/1/1", 0.41650390625)]
-        [TestCase(TestDatabaseName.TwoSecTwoBank, Totals.All, "2012/1/1", "2019/1/1", 0.42822265625)]
+        [TestCase(TestDatabaseName.TwoSecTwoBank, Totals.All, "2010 /1/1", "2019/1/1", 0.07373046875)]
+        [TestCase(TestDatabaseName.TwoSecTwoBank, Totals.All, "2011/1/1", "2019/1/1", 0.07373046875)]
+        [TestCase(TestDatabaseName.TwoSecTwoBank, Totals.All, "2012/1/1", "2019/1/1", 0.07080078125)]
         [TestCase(TestDatabaseName.TwoSecTwoBank, Totals.All, "2013/1/1", "2019/1/1", 0.095018679818698937)]
         [TestCase(TestDatabaseName.TwoSecTwoBank, Totals.All, "2014/1/1", "2019/1/1", 0.13186306269750059)]
-        [TestCase(TestDatabaseName.TwoSecTwoBank, Totals.Security, "2010 /1/1", "2019/1/1", 0.79150390625)]
-        [TestCase(TestDatabaseName.TwoSecTwoBank, Totals.Security, "2011/1/1", "2019/1/1", 0.41650390625)]
-        [TestCase(TestDatabaseName.TwoSecTwoBank, Totals.Security, "2012/1/1", "2019/1/1", 0.42822265625)]
+        [TestCase(TestDatabaseName.TwoSecTwoBank, Totals.Security, "2010 /1/1", "2019/1/1", 0.07373046875)]
+        [TestCase(TestDatabaseName.TwoSecTwoBank, Totals.Security, "2011/1/1", "2019/1/1", 0.07373046875)]
+        [TestCase(TestDatabaseName.TwoSecTwoBank, Totals.Security, "2012/1/1", "2019/1/1", 0.07080078125)]
         [TestCase(TestDatabaseName.TwoSecTwoBank, Totals.Security, "2013/1/1", "2019/1/1", 0.095018679818698937)]
         [TestCase(TestDatabaseName.TwoSecTwoBank, Totals.Security, "2014/1/1", "2019/1/1", 0.13186306269750059)]
         [TestCase(TestDatabaseName.TwoSecTwoBank, Totals.BankAccount, "2010 /1/1", "2019/1/1", -0.025712539370376319)]
