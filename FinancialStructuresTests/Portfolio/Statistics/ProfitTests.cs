243e9bb8ac79a7bb6be08776609473640796928a
Effanville <>
2021-01-27 22:51:41 +0000

    Update numeric values following changes in Investments in test databases.

--- FinancialStructuresTests/Portfolio/Statistics/ProfitTests.cs
+++ FinancialStructuresTests/Portfolio/Statistics/ProfitTests.cs
@@ -12,8 +12,8 @@ namespace FinancialStructures.Tests.Database.Statistics
         [TestCase(TestDatabaseName.OneBank, Account.Security, NameOrder.Default, double.NaN)]
         [TestCase(TestDatabaseName.OneBank, Account.BankAccount, NameOrder.Default, 1.0999999999999943)]
         [TestCase(TestDatabaseName.TwoSecTwoBank, Account.All, NameOrder.Default, double.NaN)]
-        [TestCase(TestDatabaseName.TwoSecTwoBank, Account.Security, NameOrder.Default, 555.04999999999995)]
-        [TestCase(TestDatabaseName.TwoSec, Account.Security, NameOrder.Default, 555.04999999999995)]
+        [TestCase(TestDatabaseName.TwoSecTwoBank, Account.Security, NameOrder.Default, 456.04999999999995)]
+        [TestCase(TestDatabaseName.TwoSec, Account.Security, NameOrder.Default, 456.04999999999995)]
         [TestCase(TestDatabaseName.TwoBankCur, Account.Currency, NameOrder.Default, 0.017699999999999994)]
         public void ProfitTest(TestDatabaseName databaseName, Account totals, NameOrder order, double expectedValue)
         {
@@ -24,10 +24,10 @@ namespace FinancialStructures.Tests.Database.Statistics
         [TestCase(TestDatabaseName.OneBank, Totals.All, 1.0999999999999943)]
         [TestCase(TestDatabaseName.OneBank, Totals.Security, 0.0)]
         [TestCase(TestDatabaseName.OneBank, Totals.BankAccount, 1.0999999999999943)]
-        [TestCase(TestDatabaseName.TwoSecTwoBank, Totals.All, 25983.299999999999)]
-        [TestCase(TestDatabaseName.TwoSecTwoBank, Totals.Security, 26081.099999999999)]
-        [TestCase(TestDatabaseName.TwoSec, Totals.Security, 26081.099999999999)]
-        [TestCase(TestDatabaseName.TwoSecCur, Totals.Security, 3023.2716572779873)]
+        [TestCase(TestDatabaseName.TwoSecTwoBank, Totals.All, 2843.3400000000001)]
+        [TestCase(TestDatabaseName.TwoSecTwoBank, Totals.Security, 2941.1400000000003)]
+        [TestCase(TestDatabaseName.TwoSec, Totals.Security, 2941.1400000000003)]
+        [TestCase(TestDatabaseName.TwoSecCur, Totals.Security, 1042.4239939064075)]
         public void TotalProfitTests(TestDatabaseName databaseName, Totals totals, double expectedValue)
         {
             var portfolio = TestDatabase.Databases[databaseName];
