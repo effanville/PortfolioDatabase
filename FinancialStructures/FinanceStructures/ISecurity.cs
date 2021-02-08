71e9bdef4c3ee542cc26677b65487ad32ee2f051
Effanville <>
2021-02-08 20:15:14 +0000

    Remove unnecessary duplicates in interfaces for ExchangableValueList

--- FinancialStructures/FinanceStructures/ISecurity.cs
+++ FinancialStructures/FinanceStructures/ISecurity.cs
@@ -47,43 +47,6 @@ namespace FinancialStructures.FinanceStructures
         new ISecurity Copy();
 
         /// <summary>
-        /// The first value and date stored in this security.
-        /// </summary>
-        /// <param name="currency">An optional currency to transfer the value using.</param>
-        DailyValuation FirstValue(ICurrency currency);
-
-        /// <summary>
-        /// The latest value and date stored in the value list.
-        /// </summary>
-        /// <param name="currency">An optional currency to transfer the value using.</param>
-        DailyValuation LatestValue(ICurrency currency);
-
-        /// <summary>
-        /// Gets the value on the specific date specified.
-        /// This is a linearly interpolated value from those values provided,
-        /// with the initial value if date is less that the first value.
-        /// </summary>
-        /// <param name="date">The date to query the value on.</param>
-        /// <param name="currency">An optional currency to transfer the value using.</param>
-        DailyValuation Value(DateTime date, ICurrency currency);
-
-        /// <summary>
-        /// Returns the most recent value to <paramref name="date"/> that is prior to that date.
-        /// This value is strictly prior to <paramref name="date"/>.
-        /// </summary>
-        /// <param name="date">The date to query the value on.</param>
-        /// <param name="currency">An optional currency to transfer the value using.</param>
-        DailyValuation RecentPreviousValue(DateTime date, ICurrency currency);
-
-        /// <summary>
-        /// Returns the latest valuation on or before the date <paramref name="date"/>.
-        /// This value can be on the date <paramref name="date"/>.
-        /// </summary>
-        /// <param name="date">The date to query the value for.</param>
-        /// <param name="currency">An optional currency to exchange the value with.</param>
-        DailyValuation NearestEarlierValuation(DateTime date, ICurrency currency);
-
-        /// <summary>
         /// Produces a list of data for visual display purposes. Display in the base currency
         /// of the fund ( so this does not modify values due to currency)
         /// </summary>
@@ -145,5 +108,10 @@ namespace FinancialStructures.FinanceStructures
         /// <param name="reportLogger">An optional logger to log progress.</param>
         /// <returns>Was adding or editing successful.</returns>
         bool TryAddOrEditData(DateTime oldDate, DateTime date, double unitPrice, double shares, double investment, IReportLogger reportLogger = null);
+
+        /// <summary>
+        ///
+        /// </summary>
+        void CleanData();
     }
 }
