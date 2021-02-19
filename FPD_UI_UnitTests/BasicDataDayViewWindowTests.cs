3797a01d9bafcf825ba806b8f74aa8571a49e206
Effanville <>
2021-02-19 23:00:28 +0000

    Update unit tests for ui changes.

--- FPD_UI_UnitTests/BasicDataDayViewWindowTests.cs
+++ FPD_UI_UnitTests/BasicDataDayViewWindowTests.cs
@@ -12,23 +12,39 @@ namespace FPD_UI_UnitTests
     public class BasicDataViewWindowTests
     {
         /// <summary>
+        /// Ensures the window displays data if the underlying database is modified.
+        /// </summary>
+        [Test]
+        public void EmptyPortfolioHasEmptyData()
+        {
+            // Setup basic data in the display.
+            Portfolio portfolio = new Portfolio();
+            BasicDataViewModel viewModel = new BasicDataViewModel(portfolio);
+
+            Assert.Multiple(() =>
+            {
+                Assert.IsFalse(viewModel.HasValues);
+                Assert.AreEqual(viewModel.PortfolioNameText, "Unsaved database loaded");
+            });
+        }
+
+        /// <summary>
         /// Ensures that the window displays data on loading.
         /// </summary>
         [Test]
         public void CanViewData()
         {
             BasicDataViewModel viewModel = new BasicDataViewModel(TestingGUICode.CreateBasicDataBase());
+            Assert.Multiple(() =>
+            {
+                Assert.IsTrue(viewModel.HasValues);
+                Assert.AreEqual("Portfolio: TestFilePath loaded.", viewModel.PortfolioNameText);
+                Assert.AreEqual("Total Securities: 1", viewModel.SecurityTotalText);
+                Assert.AreEqual("Total Value: 1 ", viewModel.SecurityAmountText);
 
-            Assert.AreEqual(1, viewModel.FundNames.Count);
-            Assert.AreEqual("China", viewModel.FundNames.Single().Name);
-            Assert.AreEqual("Fidelity", viewModel.FundNames.Single().Company);
-            Assert.AreEqual(1, viewModel.AccountNames.Count);
-            Assert.AreEqual("currentAccount", viewModel.AccountNames.Single().Name);
-            Assert.AreEqual("Barclays", viewModel.AccountNames.Single().Company);
-            Assert.AreEqual(1, viewModel.CurrencyNames.Count);
-            Assert.AreEqual("GBP", viewModel.CurrencyNames.Single().Name);
-            Assert.AreEqual(1, viewModel.SectorNames.Count);
-            Assert.AreEqual("UK", viewModel.SectorNames.Single().Name);
+                Assert.AreEqual("Total Bank Accounts: 1", viewModel.BankAccountTotalText);
+                Assert.AreEqual("Total Value: 1 ", viewModel.BankAccountAmountText);
+            });
         }
 
         /// <summary>
@@ -41,21 +57,22 @@ namespace FPD_UI_UnitTests
             Portfolio portfolio = new Portfolio();
             BasicDataViewModel viewModel = new BasicDataViewModel(portfolio);
 
+            Assert.IsFalse(viewModel.HasValues);
             // Now update that data.
             TestingGUICode.UpdatePortfolio(portfolio);
             viewModel.UpdateData(portfolio);
 
             // Ensure new data has been displayed correctly.
-            Assert.AreEqual(1, viewModel.FundNames.Count);
-            Assert.AreEqual("China", viewModel.FundNames.Single().Name);
-            Assert.AreEqual("Fidelity", viewModel.FundNames.Single().Company);
-            Assert.AreEqual(1, viewModel.AccountNames.Count);
-            Assert.AreEqual("currentAccount", viewModel.AccountNames.Single().Name);
-            Assert.AreEqual("Barclays", viewModel.AccountNames.Single().Company);
-            Assert.AreEqual(1, viewModel.CurrencyNames.Count);
-            Assert.AreEqual("GBP", viewModel.CurrencyNames.Single().Name);
-            Assert.AreEqual(1, viewModel.SectorNames.Count);
-            Assert.AreEqual("UK", viewModel.SectorNames.Single().Name);
+            Assert.Multiple(() =>
+            {
+
+                Assert.IsTrue(viewModel.HasValues);
+                Assert.AreEqual("Total Securities: 1", viewModel.SecurityTotalText);
+                Assert.AreEqual("Total Value: 1 ", viewModel.SecurityAmountText);
+
+                Assert.AreEqual("Total Bank Accounts: 1", viewModel.BankAccountTotalText);
+                Assert.AreEqual("Total Value: 1 ", viewModel.BankAccountAmountText);
+            });
         }
     }
 }
