9995caee8f3a989c6782303cc1a0569da5035f84
Matthew Egginton <matt.egginton@gmail.com>
2021-04-06 21:54:23 +0100

    Migrate test helpers to separate folder.

---
+++ FinancePortfolioDatabase.Tests/TestHelpers/DataNamesViewTestHelper.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using FinancialStructures.Database;
+using FinancePortfolioDatabase.Tests.TestConstruction;
+using Moq;
+using NUnit.Framework;
+using UICommon.Services;
+using System.IO.Abstractions;
+using FinancePortfolioDatabase.GUI.ViewModels.Common;
+using FinancialStructures.NamingStructures;
+using StructureCommon.DisplayClasses;
+using System.Linq;
+using System.Windows.Controls;
+
+namespace FinancePortfolioDatabase.Tests
+{
+    public abstract class DataNamesViewTestHelper
+    {
+        private Action<Action<IPortfolio>> DataUpdater
+        {
+            get
+            {
+                return action => action(Portfolio);
+            }
+        }
+
+        private IPortfolio fPortfolio;
+
+        protected IPortfolio Portfolio
+        {
+            get
+            {
+                return fPortfolio;
+            }
+            set
+            {
+                fPortfolio = value;
+                ViewModel?.UpdateData(fPortfolio);
+            }
+        }
+
+        protected DataNamesViewModel ViewModel
+        {
+            get;
+            private set;
+        }
+
+        protected Account AccountType
+        {
+            get;
+            set;
+        } = Account.Security;
+
+        public NameData AddNewItem()
+        {
+            SelectItem(null);
+            ViewModel.AddDefaultDataCommand?.Execute(new AddingNewItemEventArgs());
+            ViewModel.DataNames.Add(new SelectableEquatable<NameData>(new NameData(), false));
+            var newItem = ViewModel.DataNames.Last();
+            SelectItem(newItem.Instance);
+            BeginEdit();
+
+            return newItem.Instance;
+        }
+
+        public void SelectItem(NameData name)
+        {
+            ViewModel.SelectionChangedCommand?.Execute(new SelectableEquatable<NameData>(name, false));
+        }
+
+        public void BeginEdit()
+        {
+            ViewModel.PreEditCommand?.Execute(null);
+        }
+
+        public void CompleteEdit()
+        {
+            ViewModel.CreateCommand?.Execute(null);
+        }
+
+        public void DownloadSelected()
+        {
+            ViewModel.DownloadCommand.Execute(null);
+        }
+
+        public void DeleteSelected()
+        {
+            ViewModel.DeleteCommand?.Execute(null);
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            Mock<IFileInteractionService> fileMock = TestingGUICode.CreateFileMock("nothing");
+            Mock<IDialogCreationService> dialogMock = TestingGUICode.CreateDialogMock();
+            Portfolio = TestingGUICode.CreateEmptyDataBase();
+
+            UiGlobals globals = TestingGUICode.CreateGlobalsMock(new FileSystem(), fileMock.Object, dialogMock.Object);
+            ViewModel = new DataNamesViewModel(Portfolio, DataUpdater, TestingGUICode.DummyReportLogger, obj => { }, AccountType);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            ViewModel = null;
+            Portfolio.Clear();
+        }
+    }
+}
