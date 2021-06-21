f0d7da59918c411f2210e47d3bd71674f0857c37
Effanville <>
2021-06-21 21:21:46 +0100

    Fix unit tests due to reporting window changes.

--- FinancePortfolioDatabase.Tests/ReportingWindowTests.cs
+++ FinancePortfolioDatabase.Tests/ReportingWindowTests.cs
@@ -21,7 +21,7 @@ namespace FinancePortfolioDatabase.Tests
             viewModel.Reports.AddErrorReport(ReportSeverity.Critical, ReportType.Error, ReportLocation.Unknown, "Is this added?");
             viewModel.SyncReports();
 
-            Assert.AreEqual(1, viewModel.ReportsToView.Count, "Viewable reports should have one report added.");
+            Assert.AreEqual(1, viewModel.ReportsToView.Count(), "Viewable reports should have one report added.");
             Assert.AreEqual(ReportType.Error, viewModel.ReportsToView.Single().ErrorType);
             Assert.AreEqual(ReportLocation.Unknown, viewModel.ReportsToView.Single().ErrorLocation);
             Assert.AreEqual("Is this added?", viewModel.ReportsToView.Single().Message);
@@ -37,7 +37,7 @@ namespace FinancePortfolioDatabase.Tests
             viewModel.UpdateReport(ReportSeverity.Useful, ReportType.Error, ReportLocation.Unknown, "Is this added?");
 
             Assert.AreEqual(1, viewModel.Reports.GetReports().Count, "Reports should have a report added.");
-            Assert.AreEqual(1, viewModel.ReportsToView.Count, "Viewable reports should have one report added.");
+            Assert.AreEqual(1, viewModel.ReportsToView.Count(), "Viewable reports should have one report added.");
             Assert.AreEqual(ReportType.Error, viewModel.ReportsToView.Single().ErrorType);
             Assert.AreEqual(ReportLocation.Unknown, viewModel.ReportsToView.Single().ErrorLocation);
             Assert.AreEqual("Is this added?", viewModel.ReportsToView.Single().Message);
@@ -54,11 +54,11 @@ namespace FinancePortfolioDatabase.Tests
             viewModel.UpdateReport(ReportSeverity.Useful, ReportType.Error, ReportLocation.Unknown, "Is this also added?");
 
             Assert.AreEqual(2, viewModel.Reports.GetReports().Count, "Reports should have a report added.");
-            Assert.AreEqual(2, viewModel.ReportsToView.Count, "Viewable reports should have one report added.");
+            Assert.AreEqual(2, viewModel.ReportsToView.Count(), "Viewable reports should have one report added.");
 
             viewModel.ClearReportsCommand.Execute(3);
             Assert.AreEqual(0, viewModel.Reports.GetReports().Count, "Reports should have been cleared.");
-            Assert.AreEqual(0, viewModel.ReportsToView.Count, "Viewable reports should have been cleared.");
+            Assert.AreEqual(0, viewModel.ReportsToView.Count(), "Viewable reports should have been cleared.");
         }
 
         /// <summary>
@@ -73,12 +73,12 @@ namespace FinancePortfolioDatabase.Tests
             viewModel.UpdateReport(ReportSeverity.Useful, ReportType.Error, ReportLocation.Unknown, "Is this also added?");
 
             Assert.AreEqual(2, viewModel.Reports.GetReports().Count, "Reports should have a report added.");
-            Assert.AreEqual(2, viewModel.ReportsToView.Count, "Viewable reports should have one report added.");
+            Assert.AreEqual(2, viewModel.ReportsToView.Count(), "Viewable reports should have one report added.");
 
             viewModel.IndexToDelete = 1;
             viewModel.ClearSingleReportCommand.Execute(3);
             Assert.AreEqual(1, viewModel.Reports.GetReports().Count, "Reports should have been cleared.");
-            Assert.AreEqual(1, viewModel.ReportsToView.Count, "Viewable reports should have been cleared.");
+            Assert.AreEqual(1, viewModel.ReportsToView.Count(), "Viewable reports should have been cleared.");
             Assert.AreEqual(ReportType.Error, viewModel.ReportsToView.Single().ErrorType);
             Assert.AreEqual(ReportLocation.Unknown, viewModel.ReportsToView.Single().ErrorLocation);
             Assert.AreEqual("Is this added?", viewModel.ReportsToView.Single().Message);
