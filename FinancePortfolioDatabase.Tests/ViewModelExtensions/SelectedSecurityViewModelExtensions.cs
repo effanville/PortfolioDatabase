3e82698b00b932b9b5f67e0a394d26cacb298ded
Matthew Egginton <matt.egginton@gmail.com>
2021-08-17 22:30:54 +0100

    Add unit tests for new trade list.

--- FinancePortfolioDatabase.Tests/ViewModelExtensions/SelectedSecurityViewModelExtensions.cs
+++ FinancePortfolioDatabase.Tests/ViewModelExtensions/SelectedSecurityViewModelExtensions.cs
@@ -7,6 +7,7 @@ using Common.Structure.DataStructures;
 using FinancePortfolioDatabase.GUI.ViewModels.Common;
 using FinancePortfolioDatabase.GUI.ViewModels.Security;
 using FinancialStructures.Database;
+using FinancialStructures.DataStructures;
 
 namespace FinancePortfolioDatabase.Tests.ViewModelExtensions
 {
@@ -15,19 +16,55 @@ namespace FinancePortfolioDatabase.Tests.ViewModelExtensions
     /// </summary>
     public static class SelectedSecurityViewModelExtensions
     {
-        public static DailyValuation AddNewItem(this SelectedSecurityViewModel viewModel)
+        public static SecurityTrade AddNewTrade(this SelectedSecurityViewModel viewModel)
         {
-            viewModel.SelectItem(null);
+            viewModel.SelectTrade(null);
             viewModel.AddDefaultDataCommand?.Execute(new AddingNewItemEventArgs());
-            viewModel.TLVM.Valuations.Add(new DailyValuation());
-            var newItem = viewModel.TLVM.Valuations.Last();
-            viewModel.SelectItem(newItem);
+            viewModel.Trades.Add(new SecurityTrade());
+            var newItem = viewModel.Trades.Last();
+            viewModel.SelectTrade(newItem);
             viewModel.BeginEdit();
 
             return newItem;
         }
 
-        public static void SelectItem(this SelectedSecurityViewModel viewModel, DailyValuation valueToSelect)
+        public static void SelectTrade(this SelectedSecurityViewModel viewModel, SecurityTrade valueToSelect)
+        {
+            viewModel.SelectionChangedCommand?.Execute(valueToSelect);
+        }
+
+        public static void BeginEditTrade(this SelectedSecurityViewModel viewModel)
+        {
+            viewModel.PreEditCommand?.Execute(null);
+        }
+
+        public static void CompleteEditTrade(this SelectedSecurityViewModel viewModel, IPortfolio portfolio)
+        {
+            viewModel.AddEditDataCommand?.Execute(null);
+            viewModel.UpdateData(portfolio);
+        }
+
+        [STAThread]
+        public static void DeleteSelectedTrade(this SelectedSecurityViewModel viewModel, IPortfolio portfolio)
+        {
+            KeyEventArgs eventArgs = new KeyEventArgs(Keyboard.PrimaryDevice, new HwndSource(0, 0, 0, 0, 0, "", IntPtr.Zero), 0, Key.Delete);
+            viewModel.DeleteTradeKeyDownCommand?.Execute(eventArgs);
+            viewModel.UpdateData(portfolio);
+        }
+
+        public static DailyValuation AddNewUnitPrice(this SelectedSecurityViewModel viewModel)
+        {
+            viewModel.SelectUnitPrice(null);
+            viewModel.TLVM.AddDefaultDataCommand?.Execute(new AddingNewItemEventArgs());
+            viewModel.TLVM.Valuations.Add(new DailyValuation());
+            var newItem = viewModel.TLVM.Valuations.Last();
+            viewModel.SelectUnitPrice(newItem);
+            viewModel.BeginEdit();
+
+            return newItem;
+        }
+
+        public static void SelectUnitPrice(this SelectedSecurityViewModel viewModel, DailyValuation valueToSelect)
         {
             viewModel.TLVM.SelectionChangedCommand?.Execute(valueToSelect);
         }
