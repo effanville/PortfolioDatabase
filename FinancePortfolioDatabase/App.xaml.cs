953b8390494208057f28348e901cde47682ead67
Effanville <>
2021-09-08 17:56:17 +0100

    Ensure if error is null then doesnt throw when recording issues.

--- FinancePortfolioDatabase/App.xaml.cs
+++ FinancePortfolioDatabase/App.xaml.cs
@@ -56,7 +56,7 @@ namespace FinancePortfolioDatabase
             //Handling the exception within the UnhandledExcpeiton handler.
             _ = MessageBox.Show(e?.Exception?.Message + Environment.NewLine + e?.Exception?.StackTrace, "Exception Caught", MessageBoxButton.OK, MessageBoxImage.Error);
             var main = Current.MainWindow as MainWindow;
-            main.PrintErrorLog(e.Exception);
+            main.PrintErrorLog(e?.Exception);
             e.Handled = true;
         }
 
