227601456dda93ff3d028301dd6982a63e0216e5
Effanville <>
2021-10-12 00:54:24 +0100

    Add stock statistics to repo.

---
+++ FinancialStructures/StockStructures/Statistics/Implementation/ADXStat.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace FinancialStructures.StockStructures.Statistics.Implementation
+{
+    internal class ADXStat : IStockStatistic
+    {
+        /// <inheritdoc/>
+        public int BurnInTime
+        {
+            get;
+        }
+
+        /// <inheritdoc/>
+        public StockStatisticType TypeOfStatistic
+        {
+            get;
+        }
+
+        /// <inheritdoc/>
+        public StockDataStream DataType => StockDataStream.None;
+
+        public ADXStat(int numberDays, StockStatisticType statisticType)
+        {
+            BurnInTime = numberDays;
+            TypeOfStatistic = statisticType;
+        }
+
+        /// <inheritdoc/>
+        public double Calculate(DateTime date, IStock stock)
+        {
+            return stock.ADX(date, BurnInTime);
+        }
+    }
+}
