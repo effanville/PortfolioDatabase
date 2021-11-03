d5fc9ce8d427df7fa54a5865cf7358ecbb014cc8
Effanville <>
2021-11-03 22:31:59 +0000

    Ensure can download from Yahoo.

--- FinancialStructures/Database/Download/Portfolio.Download.cs
+++ FinancialStructures/Database/Download/Portfolio.Download.cs
@@ -245,7 +245,7 @@ namespace FinancialStructures.Database.Download
 
                 return false;
             };
-            int number = 30;
+            int number = 28;
             string searchString = $"data-reactid=\"{number}\"><span class=\"Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)\" data-reactid=\"{number + 1}\">";
             int poundsValue = data.IndexOf(searchString);
             if (poundsValue != -1)
