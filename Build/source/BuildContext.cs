0b3e8a13362f3c82bf5afa696a13808fd71c0afb
Effanville <>
2022-02-09 23:52:32 +0000

    Update build process to read configuration from config file.

--- Build/source/BuildContext.cs
+++ Build/source/BuildContext.cs
@@ -25,15 +25,21 @@ namespace Build
             get;
         }
 
-        public string RemoteDir
+        private string SolutionFileName
         {
             get;
         }
 
-        public (string FolderName, string ProjectName)[] NugetPublishProjects
+        public ProjectInfo[] NugetPublishProjects
         {
             get;
         }
+
+        public ProjectInfo[] ExeProjects
+        {
+            get;
+        }
+
 
         #endregion
 
@@ -60,15 +66,18 @@ namespace Build
         public BuildContext(ICakeContext context)
             : base(context)
         {
-            BuildConfiguration = context.Arguments.GetArgument("configuration") ?? Configurations.DefaultBuildConfiguration;
-            Framework = context.Arguments.GetArgument("framework") ?? Configurations.DefaultFramework;
-            string publishDir = context.Arguments.GetArgument("publishDir") ?? $"{Configurations.DefaultPublishDir}/{Configurations.SolutionName}/{Framework}";
-            string nugetPublishDir = context.Arguments.GetArgument("publishDir") ?? $"{Configurations.DefaultPublishDir}/{Configurations.SolutionName}";
-            Runtime = context.Arguments.GetArgument("runtime") ?? Configurations.DefaultRuntime;
-            RemoteDir = context.Arguments.GetArgument("remote") ?? string.Empty;
+            RepoDir = context.MakeAbsolute(context.Directory("../../"));
+            var config = UserConfiguration.ReadConfig(RepoDir);
+
+            SolutionFileName = config.SolutionFileName;
+
+            BuildConfiguration = context.Arguments.GetArgument("configuration") ?? config.DefaultBuildConfiguration;
+            Framework = context.Arguments.GetArgument("framework") ?? config.DefaultFramework;
+            string publishDir = context.Arguments.GetArgument("publishDir") ?? $"{config.DefaultPublishDir}/{config.SolutionName}/{Framework}";
+            string nugetPublishDir = context.Arguments.GetArgument("publishDir") ?? $"{config.DefaultPublishDir}/{config.SolutionName}";
+            Runtime = context.Arguments.GetArgument("runtime") ?? config.DefaultRuntime;
             IsProductionBuild = context.Arguments.HasArgument("isProd");
 
-            RepoDir = context.MakeAbsolute(context.Directory("../../"));
             PublishLocation = RepoDir + context.Directory($"{publishDir}");
             NugetPublishLocation = RepoDir + context.Directory($"{nugetPublishDir}");
 
@@ -76,21 +85,36 @@ namespace Build
             if (!string.IsNullOrWhiteSpace(nugetPackages))
             {
                 string[] projects = nugetPackages.Split(',');
-                NugetPublishProjects = new (string FolderName, string ProjectName)[projects.Length];
+                NugetPublishProjects = new ProjectInfo[projects.Length];
                 for (int i = 0; i < projects.Length; i++)
                 {
-                    NugetPublishProjects[i] = (projects[i], projects[i]);
+                    NugetPublishProjects[i] = new ProjectInfo(projects[i], projects[i]);
                 }
             }
             else
             {
-                NugetPublishProjects = Configurations.NugetPackageProjects;
+                NugetPublishProjects = config.NugetPackageProjects;
+            }
+
+            string exeProjects = context.Arguments.GetArgument("exe");
+            if (!string.IsNullOrWhiteSpace(exeProjects))
+            {
+                string[] projects = exeProjects.Split(',');
+                ExeProjects = new ProjectInfo[projects.Length];
+                for (int i = 0; i < projects.Length; i++)
+                {
+                    ExeProjects[i] = new ProjectInfo(projects[i], projects[i]);
+                }
+            }
+            else
+            {
+                ExeProjects = config.ExecutablePublishProjects;
             }
         }
 
         public FilePath SolutionFilePath()
         {
-            return RepoDir.CombineWithFilePath(Configurations.SolutionFileName);
+            return RepoDir.CombineWithFilePath(SolutionFileName);
         }
 
         public FilePath NugetConfigFilePath()
@@ -101,9 +125,9 @@ namespace Build
         public (string ProjectName, FilePath FilePath)[] ExecutableProjectFilePaths()
         {
             var filePaths = new List<(string ProjectName, FilePath)>();
-            foreach (var (folderName, projectName) in Configurations.ExecutablePublishProjects)
+            foreach (var info in ExeProjects)
             {
-                filePaths.Add((projectName, ((DirectoryPath)(RepoDir + this.Directory($"{folderName}"))).CombineWithFilePath($"{projectName}.csproj")));
+                filePaths.Add((info.ProjectName, ((DirectoryPath)(RepoDir + this.Directory($"{info.FolderName}"))).CombineWithFilePath($"{info.ProjectName}.csproj")));
             }
 
             return filePaths.ToArray();
@@ -112,9 +136,9 @@ namespace Build
         public (string ProjectName, FilePath FilePath)[] NugetPackageProjectFilePaths()
         {
             var filePaths = new List<(string, FilePath)>();
-            foreach (var (folderName, projectName) in NugetPublishProjects)
+            foreach (var info in NugetPublishProjects)
             {
-                filePaths.Add((projectName, ((DirectoryPath)(RepoDir + this.Directory($"{folderName}"))).CombineWithFilePath($"{projectName}.csproj")));
+                filePaths.Add((info.ProjectName, ((DirectoryPath)(RepoDir + this.Directory($"{info.FolderName}"))).CombineWithFilePath($"{info.ProjectName}.csproj")));
             }
 
             return filePaths.ToArray();
