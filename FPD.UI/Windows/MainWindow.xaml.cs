5f6bf49fb3ea240e1970d2fcfa3b88955adf7e78
Effanville <>
2023-06-13 08:07:26 +0800

    Remove and implement custom window chrome.

--- FPD.UI/Windows/MainWindow.xaml.cs
+++ FPD.UI/Windows/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using FPD.Logic.ViewModels;
 using FPD.Logic.ViewModels.Common;
 using FinancialStructures.Database;
 using Common.UI.Wpf;
+using System.Windows.Input;
+using System.Windows.Shapes;
 
 namespace FPD.UI.Windows
 {
@@ -27,6 +29,7 @@ namespace FPD.UI.Windows
         /// </summary>
         public MainWindow()
         {
+            bool isLightTheme = true;
             FileInteractionService FileInteractionService = new FileInteractionService(this);
             DialogCreationService DialogCreationService = new DialogCreationService(this);
             fUiGlobals = new UiGlobals(null, new DispatcherInstance(), new FileSystem(), FileInteractionService, DialogCreationService, null);
@@ -102,6 +105,95 @@ namespace FPD.UI.Windows
                 if (VM.Tabs[MainTabControl.SelectedIndex] is DataDisplayViewModelBase vmBase && vmBase.Closable)
                 {
                     VM.Tabs.RemoveAt(MainTabControl.SelectedIndex);
+                }
+            }
+        }
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e) => App.Current.MainWindow.WindowState = WindowState.Minimized;
+
+        private void MaximizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (App.Current.MainWindow.WindowState == WindowState.Maximized)
+            {
+                App.Current.MainWindow.WindowState = WindowState.Normal;
+            }
+            else if (App.Current.MainWindow.WindowState == WindowState.Normal)
+            {
+                App.Current.MainWindow.WindowState = WindowState.Maximized;
+            }
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e) => Application.Current.MainWindow.Close();
+
+        private void UserControl_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            App.Current.MainWindow.DragMove();
+        }
+
+        bool ResizeInProcess = false;
+        private void Resize_Init(object sender, MouseButtonEventArgs e)
+        {
+            Rectangle senderRect = sender as Rectangle;
+            if (senderRect != null)
+            {
+                ResizeInProcess = true;
+                senderRect.CaptureMouse();
+            }
+        }
+
+        private void Resize_End(object sender, MouseButtonEventArgs e)
+        {
+            Rectangle senderRect = sender as Rectangle;
+            if (senderRect != null)
+            {
+                ResizeInProcess = false; ;
+                senderRect.ReleaseMouseCapture();
+            }
+        }
+
+        private void Resizeing_Form(object sender, MouseEventArgs e)
+        {
+            if (ResizeInProcess)
+            {
+                double temp = 0;
+                Rectangle senderRect = sender as Rectangle;
+                Window mainWindow = senderRect.Tag as Window;
+                if (senderRect != null)
+                {
+                    double width = e.GetPosition(mainWindow).X;
+                    double height = e.GetPosition(mainWindow).Y;
+                    senderRect.CaptureMouse();
+                    if (senderRect.Name.Contains("right", StringComparison.OrdinalIgnoreCase))
+                    {
+                        width += 5;
+                        if (width > 0)
+                            mainWindow.Width = width;
+                    }
+                    if (senderRect.Name.Contains("left", StringComparison.OrdinalIgnoreCase))
+                    {
+                        width -= 5;
+                        temp = mainWindow.Width - width;
+                        if ((temp > mainWindow.MinWidth) && (temp < mainWindow.MaxWidth))
+                        {
+                            mainWindow.Width = temp;
+                            mainWindow.Left += width;
+                        }
+                    }
+                    if (senderRect.Name.Contains("bottom", StringComparison.OrdinalIgnoreCase))
+                    {
+                        height += 5;
+                        if (height > 0)
+                            mainWindow.Height = height;
+                    }
+                    if (senderRect.Name.ToLower().Contains("top", StringComparison.OrdinalIgnoreCase))
+                    {
+                        height -= 5;
+                        temp = mainWindow.Height - height;
+                        if ((temp > mainWindow.MinHeight) && (temp < mainWindow.MaxHeight))
+                        {
+                            mainWindow.Height = temp;
+                            mainWindow.Top += height;
+                        }
+                    }
                 }
             }
         }
