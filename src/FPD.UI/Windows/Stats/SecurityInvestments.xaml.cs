8f5964fe2ba1e4c6deef8e13e41b823f3e0e31ec
Effanville <effanville@users.noreply.github.com>
2026-03-25 20:38:58 +0800

    fix: remove unnecessary date formatting in security view

--- src/FPD.UI/Windows/Stats/SecurityInvestments.xaml.cs
+++ src/FPD.UI/Windows/Stats/SecurityInvestments.xaml.cs
@@ -1,15 +1,9 @@
-﻿using Effanville.Common.UI.Wpf.Controls;
+﻿namespace Effanville.FPD.UI.Windows.Stats;
 
-namespace Effanville.FPD.UI.Windows.Stats
+/// <summary>
+/// Interaction logic for SecurityInvestments.xaml
+/// </summary>
+internal partial class SecurityInvestments
 {
-    /// <summary>
-    /// Interaction logic for SecurityInvestments.xaml
-    /// </summary>
-    internal partial class SecurityInvestments : AutoGenColumnControl
-    {
-        public SecurityInvestments()
-        {
-            InitializeComponent();
-        }
-    }
+    public SecurityInvestments() => InitializeComponent();
 }
